74bf8521cb536ecd32a8202504a0c492d78107b6
Sergejs Glusnevs <sergejs.glusnevs@yahoo.de>
2016-04-28 15:47:30 +0200

    Password generalized

--- config.php
+++ config.php
@@ -13,6 +13,6 @@ return array(
         'hostname'  =>  'localhost',
         'database'  =>  'reports',
         'username'  =>  'reports',
-        'password'  =>  'Zhopa6Zhopa6Posmotri6NGK'
+        'password'  =>  'mypassword'
     )
 );
